6bf3667a637e531bfaab0e8fc258b87ed7999e84
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-03-07 21:00:46 +0100

    feat : début de validation de formulaire en js

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,10 @@
+"use strict";
 // DOM Elements
 
 const topNav = document.getElementById("myTopnav");
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const formData = document.querySelectorAll(".form-data");
 const closeModalIcon = document.querySelector(".close");
 const navIcon = document.querySelector(".nav-icon");
 const modalForm = document.getElementById("modal-form");
@@ -32,21 +33,50 @@ closeModalIcon.addEventListener("click", function () {
 });
 
 // Form on submit event
+
+// const fields = document.querySelectorAll("#first, #last, #email, #birthdate, #quantity, .checkbox-icon:checked, #checkbox1");
+const fields = document.querySelectorAll(".form-data input");
+console.log(fields);
+fields.forEach((field) => {
+	field.addEventListener("focus", () => resetField(field));
+	field.addEventListener("blur", () => validateField(field));
+});
+
+function validateField(field) {
+	if (field.checkValidity()) {
+		// field.removeAttribute('data-error', field.validationMessage);
+		return true;
+	} else {
+		// field.setAttribute('data-error', field.validationMessage);
+		field.classList.add("invalid");
+		let msg = document.createElement("span");
+		msg.classList.add("msg");
+		msg.innerText = field.validationMessage;
+		field.parentNode.appendChild(msg);
+		return false;
+	}
+}
+function resetField(field) {
+	if (field.classList.contains("invalid")) {
+		field.classList.remove("invalid");
+		let message = field.parentNode.lastChild;
+		message.remove();
+	}
+}
+
 modalForm.addEventListener("submit", function (e) {
-	e.preventDefault();
-	const firstname = document.getElementById("first").value;
-	const lastname = document.getElementById("last").value;
-	const email = document.getElementById("email").value;
-	const birthdate = document.getElementById("birthdate").value;
-	const quantityTournament = document.getElementById("quantity").value;
-	const placeTournament = document.querySelector('input[name="location"]:checked').value;
-	const checkboxConditions = document.getElementById("checkbox1").value;
-	console.log(firstname);
-	console.log(lastname);
-	console.log(email);
-	console.log(birthdate);
-	console.log(quantityTournament);
-	console.log(placeTournament);
-	console.log(checkboxConditions);
+	fields.forEach((field) => resetField(field));
+	let valid = true;
+
+	fields.forEach((field) => {
+		if (!validateField(field)) {
+			valid = false;
+			e.preventDefault();
+		}
+	});
+	if (valid) {
+		this.submit();
+	}
+
 	// return validate();
 });
